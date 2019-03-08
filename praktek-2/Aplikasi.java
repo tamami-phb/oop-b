855720ec2e0dc2193f25dbe580646d2fbceaff75
tamami <tamami.oka@gmail.com>
2019-03-08 17:32:16 +0700

    using another for

--- praktek-2/Aplikasi.java
+++ praktek-2/Aplikasi.java
@@ -10,5 +10,22 @@ public class Aplikasi {
         } else {
             System.out.println("Gunakan : java Aplikasi {angka}");
         }
+
+        Mahasiswa mhs[] = {
+          new Mahasiswa("19001", "tamami"),
+          new Mahasiswa("19002", "diva"),
+          new Mahasiswa("19003", "nabila")
+        };
+        System.out.println("for");
+        int i=0;
+        for(; i < mhs.length;) {
+            mhs[i].cetak();
+            i++;
+        }
+
+        System.out.println("\n\nanother for");
+        for(Mahasiswa data : mhs) {
+            data.cetak();
+        }
     }
 }
