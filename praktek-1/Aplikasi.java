d5c3bdbe4a2a99f3eb014bb890a0297bd5d26a1f
tamami <tamami.oka@gmail.com>
2019-03-08 17:03:14 +0700

    using args

--- praktek-1/Aplikasi.java
+++ praktek-1/Aplikasi.java
@@ -3,14 +3,23 @@ public class Aplikasi {
         Mahasiswa mhs1 = new Mahasiswa("19001", "tamami");
         Mahasiswa mhs2 = new Mahasiswa("19002", "diva");
         Mahasiswa mhs3 = new Mahasiswa("19003", "nabila");
+        Mahasiswa mhs4 = null;
+        if(args.length == 2) {
+            mhs4 = new Mahasiswa(args[0], args[1]);
+        }
 
         //mhs1.nim = "19001";
         //mhs1.nama = "tamami";
         //System.out.println(mhs1.nim + " : " + mhs1.nama);
         //System.out.println(mhs2.nim + " : " + mhs2.nama);
         //System.out.println(mhs3.nim + " : " + mhs3.nama);
+        //mhs1.nim = "20001";
         mhs1.cetak();
         mhs2.cetak();
         mhs3.cetak();
+
+        if(args.length == 2) {
+            mhs4.cetak();
+        }
     }
 }
