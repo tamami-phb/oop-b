9cbb380c6b57a853e108280919ec8e1fe185c430
tamami <tamami.oka@gmail.com>
2019-03-08 17:25:44 +0700

    using if and terniary

---
+++ praktek-2/Aplikasi.java
@@ -0,0 +1,14 @@
+public class Aplikasi {
+    public static void main(String[] args) {
+        int angka = 0;
+
+        if(args.length == 1) {
+            angka = Integer.parseInt(args[0]);
+            String hasil =
+                    (angka % 2 == 0) ? "genap" : "ganjil";
+            System.out.println(angka + " : " + hasil);
+        } else {
+            System.out.println("Gunakan : java Aplikasi {angka}");
+        }
+    }
+}
