9ef0f584d7f39257308373f598f558800311402a
tamami <tamami.oka@gmail.com>
2019-03-15 17:09:40 +0700

    showing window

---
+++ praktek-5-gui/Aplikasi.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Aplikasi {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(400, 250);
+        frame.setTitle("Aplikasi Q");
+        frame.setVisible(true);
+    }
+
+}
