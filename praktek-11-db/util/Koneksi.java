889e6f6c6eea9c6d38b223631abb1070596cd994
tamami <tamami.oka@gmail.com>
2019-05-10 15:28:47 +0700

    success query test

--- praktek-11-db/util/Koneksi.java
+++ praktek-11-db/util/Koneksi.java
@@ -13,6 +13,14 @@ public class Koneksi {
                 "root",
                 "rahasia"
         );
+        statement = connection.createStatement();
+        ResultSet result = statement.executeQuery("select * from mahasiswa");
+        while(result.next()) {
+            System.out.println("NIM : " + result.getString(1));
+            System.out.println("NAMA : " + result.getString(2));
+            System.out.println("KELAS : " + result.getString(3));
+        }
+        connection.close();
     }
 
 }
